a5adf12b279160ed3750cd1c329599949f5bd197
viniciusmo <viniciusoliveiravmo@gmail.com>
2017-10-05 21:32:11 -0300

    Fix build

--- Sample/src/main/java/com/luseen/spacenavigationview/MainActivity.java
+++ Sample/src/main/java/com/luseen/spacenavigationview/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         spaceNavigationView = (SpaceNavigationView) findViewById(R.id.space);
         spaceNavigationView.initWithSaveInstanceState(savedInstanceState);
         spaceNavigationView.addSpaceItem(new SpaceItem("HOME", R.drawable.near_me,R.drawable.near_me));
